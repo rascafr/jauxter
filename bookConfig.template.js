7db82f6b34e2039c519ffc18bf449ab9fc2da437
François Leparoux <fleparoux@deloitte.fr>
2021-10-06 11:52:44 +0200

    - cleanup

--- bookConfig.template.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    title: 'Jauxter booking example',
-    color: '#eb1354',
-    timeFrom: '09:30',
-    timeTo: '20:00',
-    resourceId: 12345,
-    externalInvitedPeople: '',
-    description: ''
-}
